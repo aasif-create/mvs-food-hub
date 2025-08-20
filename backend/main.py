e025ec8ff9afdb27904bfb31329f4ff2eea15993
aasif-create <aasifmmd12345@gmail.com>
2025-08-20 08:27:02 +0530

    UPDATED SIGNUP PAGE LOGIN

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, EmailStr
+from database import users_collection
+from passlib.hash import bcrypt
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Backend is running successfully 🚀"}
+class SignupRequest(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
 
+@app.get("/")
+def root():
+    return {"message": "MVS Food Hub API running"}
+
+@app.post("/signup")
+def signup(user: SignupRequest):
+    try:
+        doc = user.model_dump()
+        doc["password"] = bcrypt.hash(user.password)
+        result = users_collection.insert_one(doc)
+        return {"message": "User registered successfully", "user_id": str(result.inserted_id)}
+    except Exception as e:
+        return {"error": str(e)}
